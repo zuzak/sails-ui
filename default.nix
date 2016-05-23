26f1a15f92558a37e7e9815ddac5a7859a4728b2
Louis Taylor <louis@kragniz.eu>
2016-05-23 03:13:46 +0100

    Add pyyaml to default.nix

--- default.nix
+++ default.nix
@@ -5,11 +5,12 @@ in
     python ? pkgs.python,
     pygobject3 ? pkgs.pygobject3,
     gtk3 ? pkgs.gtk3,
+    yaml ? pkgs.python27Packages.pyyaml,
   }:
 
 stdenv.mkDerivation {
   name = "sails-ui";
   version = "0.1.0";
   src = ./.;
-  buildInputs = [ python pygobject3 gtk3 ];
+  buildInputs = [ python pygobject3 gtk3 yaml ];
 }
